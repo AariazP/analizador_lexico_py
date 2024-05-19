c7f99f370d7e85e5f28169725e317bb170a91adf
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-05-19 09:26:57 -0500

    El analizador lexico usa interfaz y genera la tabla para los tokens

--- Analizador.py
+++ Analizador.py
@@ -1,160 +1,154 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from tkinter import ttk
 
+# Patrón de los simbolos que se van a validar
 patrones = [
     ('-', 'Operador de Resta'),
     ('+', 'Operador de Suma'),
     ('>', 'Operador Mayor Que (>)'),
     ('<', 'Operador Menor Que (<)'),
-    ('=', 'Operador de asignacion'),
+    ('=', 'Operador de asignación'),
     ('!=', 'Operador de diferente'),
-    ('+=', 'Operador de concatenacion '),
+    ('+=', 'Operador de concatenación '),
     ('==', 'Operador de igualdad'),
     ('&', 'Operador Y'),
     ('||', 'Operador O'),
     ('!', 'Operador para diferente '),
-    ('(', 'Parentesis (Apertura)'),
-    (')', 'Parentesis (Cierre)'),
+    ('(', 'Paréntesis (Apertura)'),
+    (')', 'Paréntesis (Cierre)'),
     ('{', 'Llave (Apertura)'),
     ('}', 'Llave (Cierre)'),
-    ('#[0-9A-F]+', 'Numero hexadecimal'),
     ('#', 'Comentario'),
     (';', 'Punto y coma'),
+    ('SUMA', 'Operador de Suma'),
+    ('MENOS', 'Operador de Resta'),
+    ('POR', 'Operador de Multiplicación'),
+    ('DIVIDIR', 'Operador de División'),
+    ('IGUAL', 'Operador de Igualdad'),
+    ('DIFERENTE', 'Operador de Diferencia'),
+    ('AND', 'Operador Lógico Y'),
+    ('OR', 'Operador Lógico O'),
+    ('PARA', 'Palabra Reservada Para Bucle'),
+    ('SI', 'Palabra Reservada Para Decisión'),
+    ('SINO', 'Palabra Reservada Para Alternativa'),
+    ('CLASE', 'Palabra Reservada Para Clase'),
+    ('int', 'Palabra Reservada Para Enteros'),
+    ('float', 'Palabra Reservada Para Reales'),
+    ('String', 'Palabra Reservada Para Cadenas de Caracteres'),
+    ('char', 'Palabra Reservada Para Caracteres'),
+    ('boolean', 'Palabra Reservada Para Booleanos')
 ]
 
+# Método que valida las palabras reservadas del lenguaje
 def es_palabra_reservada(palabra):
-    palabras_reservadas = ['para', 'mientras', 'si', 'clase', 'sino', 'interface']
-    return palabra in palabras_reservadas
+    palabras_reservadas = ['PARA', 'SI', 'SINO', 'CLASE', 'int', 'float', 'String', 'char', 'boolean']
+    return palabra.upper() in palabras_reservadas
 
-def obtener_palabra_completa(palabra):
-    palabras_completas = {'para': 'para', 'si': 'si','sino':'sino' ,'mientras': 'mientras', 'clase': 'clase',
-                          'interface': 'interface', 'publico': 'publico', 'privado': 'privado', 'protegido': 'protegido'}
-    return palabras_completas.get(palabra, palabra)
-
+# Método que valida si una cadena es un identificador y que cumpla con las reglas
 def es_identificador(palabra):
-    if palabra[0].isalpha() and palabra[1:].isalnum():
-        return True
-    return False
+    return palabra[0].isalpha() and palabra[1:].isalnum() and len(palabra) >= 5
 
+# Método que valida si un número es natural
 def es_numero_natural(palabra):
     return palabra.isdigit()
 
+# Método que valida si un número es real
 def es_numero_real(palabra):
     try:
         float(palabra)
         return True
     except ValueError:
         return False
-    
+
+# Método que valida si una cadena es una cadena de texto
 def es_cadena_texto(palabra):
-    if palabra.startswith('//"') and palabra.endswith('"//'):
-        return True
-    return False
+    return palabra.startswith('"') and palabra.endswith('"')
 
-def verificarLinea(linea):
+# Método que verifica cada línea del código
+def verificar_linea(linea):
     result = []
+    palabras = linea.split()
     
-    if '#' in linea:
-        elemento = (linea.strip(), 'Comentario')
-        result.append(elemento)
-        return result
+    for palabra in palabras:
+        tipo = 'No Reconocido'
 
-    palabras = linea.split() 
-    for palabra in palabras:        
-        if '(' in linea and ')' in linea:
-            inicio = linea.index('(')
-            fin = linea.index(')')
-            parametro = linea[inicio+1:fin]
-            nombre_funcion = linea[:inicio+1] + ')'
-            result.append((nombre_funcion.strip(), 'Funcion'))
-
-            if ',' in parametro:
-                elemento = (linea.strip(), 'Funcion con parametro de tipo lista')
-                parametros = parametro.split(',')
-                for param in parametros:
-                    result.append(("tipo del parametro", str(verificarLinea(param))))
-            else:
-                elemento = (linea.strip(), 'Funcion con parametro simple')
-                analizador_lexico(parametro)
-                result.append(("tipo del parametro", str(verificarLinea(parametro))))
-
-        for palabra in linea.split():
-            tipo = 'No Reconocido'
-
-            if palabra.startswith('//"') and palabra.endswith('"//'):
-                tipo = 'Cadena de texto'
-                continue
-
+        if palabra.startswith('#'):
+            tipo = 'Comentario'
+        else:
             for patron, tipo_definido in patrones:
                 if palabra == patron:
                     tipo = tipo_definido
                     break
 
             if es_palabra_reservada(palabra):
-                tipo = 'Palabra reservada: ' + obtener_palabra_completa(palabra)
-            elif es_cadena_texto(palabra):
-                tipo = 'Cadena de texto'
+                tipo = f'Palabra Reservada: {palabra.upper()}'
             elif es_identificador(palabra):
                 tipo = 'Identificador'
             elif es_numero_natural(palabra):
-                tipo = 'Numero Natural'
+                tipo = 'Número Natural'
             elif es_numero_real(palabra):
-                tipo = 'Numero Real'
-
-            elemento = (palabra, tipo)
-            result.append(elemento)
-
+                tipo = 'Número Real'
+            elif es_cadena_texto(palabra):
+                tipo = 'Cadena de Texto'
+        
+        result.append((palabra, tipo))
+    
     return result
 
+# Método que realiza el análisis léxico
 def analizador_lexico(codigo):
     tabla_resultados = set()
 
-    for linea_numero, linea in enumerate(codigo.split('\n'), start=1):
+    for linea in codigo.split('\n'):
         linea = linea.strip()
-        result = verificarLinea(linea)      
-
+        result = verificar_linea(linea)
+        
         for res in result:
-            tabla_resultados.add(res)  
+            tabla_resultados.add(res)
 
     return tabla_resultados
 
-def leerCodigo(path):
+# Método que lee el contenido del archivo
+def leer_codigo(path, text_widget):
     with open(path, 'r') as archivo:
         codigo = archivo.read()
 
     resultados = analizador_lexico(codigo)
-    return resultados
+    mostrar_resultados(resultados, text_widget)
 
-def mostrar_resultados(resultados):
+# Método que muestra los resultados en un widget de texto
+def mostrar_resultados(resultados, text_widget):
+    text_widget.delete(1.0, tk.END)  # Limpiar el contenido del widget de texto
+    result_str = "-" * 87 + "\n"
+    result_str += "|{:<40}  | {:<40} |\n".format("Valor", "Tipo")
+    result_str += "-" * 87 + "\n"
     for resultado in resultados:
-        tree.insert('', tk.END, values=(resultado[0], resultado[1]))
+        result_str += "| {:<40} | {:<40} |\n".format(resultado[0], resultado[1])
+    result_str += "-" * 87 + "\n"
+    text_widget.insert(tk.END, result_str)  # Insertar el contenido en el widget de texto
 
-def seleccionar_archivo():
-    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
-    if file_path:
-        try:
-            resultados = leerCodigo(file_path)
-            for row in tree.get_children():
-                tree.delete(row)
-            mostrar_resultados(resultados)
-        except Exception as e:
-            messagebox.showerror("Error", f"No se pudo analizar el archivo: {e}")
+# Método para seleccionar el archivo a analizar
+def seleccionar_archivo(text_widget):
+    path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if path:
+        leer_codigo(path, text_widget)
 
-root = tk.Tk()
-root.title("Analizador Léxico")
-root.geometry("800x600")
+# Configuración de la interfaz gráfica
+def configurar_interfaz():
+    root = tk.Tk()
+    root.title("Analizador Léxico")
+    root.geometry("800x600")
 
-frame = tk.Frame(root)
-frame.pack(pady=20)
+    label = tk.Label(root, text="Seleccione el archivo .txt para analizar:")
+    label.pack(pady=10)
 
-btn_seleccionar_archivo = tk.Button(frame, text="Seleccionar archivo .txt", command=seleccionar_archivo)
-btn_seleccionar_archivo.pack()
+    boton_seleccionar = tk.Button(root, text="Seleccionar Archivo", command=lambda: seleccionar_archivo(text_widget))
+    boton_seleccionar.pack(pady=20)
 
-columns = ('Valor', 'Tipo')
-tree = ttk.Treeview(root, columns=columns, show='headings')
-tree.heading('Valor', text='Valor')
-tree.heading('Tipo', text='Tipo')
-tree.pack(expand=True, fill='both')
+    text_widget = tk.Text(root, wrap=tk.NONE)
+    text_widget.pack(expand=True, fill=tk.BOTH)
 
-root.mainloop()
+    root.mainloop()
+
+if __name__ == '__main__':
+    configurar_interfaz()
